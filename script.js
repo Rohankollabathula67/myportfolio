2203e198b10ca4a348011fe36ebbc7b53bf8cd10
Rohan <118647297+Rohankollabathula67@users.noreply.github.com>
2025-10-17 22:14:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Instead of setting borderColor to a drop-shadow string:
+    pointer.style.filter = 'drop-shadow(0 0 8px #daa52080)';
+// Do NOT hide the system cursor (optional)
+// document.body.style.cursor = 'none';
